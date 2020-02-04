aea1f546989c9b68325d41823f6f452b20650aa8
Yves Dubois-Pèlerin <yves.dubois-pelerin@etat.ge.ch>
2020-02-04 15:48:58 +0100

    Split into several files + add data

--- README.md
+++ README.md
@@ -1,21 +1,32 @@
-gitSync is a standalone Java application which replicates the users from an LDAP source into a
-[GitLab](https://about.gitlab.com) server.
+This projet is small HTML/JavaScript page which gives graphical charts of the traffic
+("unique cloners" and "unique visitors") on the Etat de Geneve's GitHub projects.
 
-# Presentation
+This project heavily resorted to Tutorial Point's 
+[primer](https://www.tutorialspoint.com/d3js/index.htm).
 
-## Context
+# Technical facts
 
-At État de Genève we use an Active Directory server to authenticate users, plus a home-made solution
-(hereafter denoted as "the LDAP server" ) to manage authorizations.
-As is done in many organizations, the LDAP server organizes users in groups such
-as "IT-DEV-JAVA", "IT-DEV-PHP" or "FINANCE".
-Access rights to systems and applications are granted to groups or to users.
-A user is assigned to any number of groups.
+The project consists of three parts:
 
-Now GitLab comes into play.
-The server is an on-premise instance of the GitLab community edition.
-Our need is to *replicate automatically the LDAP configuration of the users onto the GitLab server*.
-For example, we want the GitLab server to acquire automatically groups "IT-DEV-JAVA', 'IT-DEV-PHP', etc.,
-from the LDAP server, as well as the members of every group.
-We also want the GitLab group members to acquire the adequate right permissions.
+- A small HTML file
+- A JavaScript file which stores the traffic values, for a set of GitHub projects
+- A JavaScript file which plots the traffic values. 
+It is based on the [D3](d3js-org) library.
 
+# Usage
+
+In order to view the charts, simply open a browser on file `charts.html`.
+
+# Customization 
+
+In order to accommodate this project to your GitHub page:
+
+1. Clone this project
+
+2. Edit file `data/XXX.js` and replace the values with you own traffic values
+
+On the community version of GitHub, the traffic values cna be found, for each project, at tab `Insights`, option
+`Traffic`. 
+Note that GitHub provides values on a 2-week time range only. 
+This implies that you have to pick and store these value quite regularly, e.g., every month.
+As far as we know, values older than two weeks are lost for ever. 
---
+++ charts.js
@@ -0,0 +1,119 @@
+plot(chvoteData, 'chvote-1.0');
+plot(chvoteProtocolPocData, 'chvote-protocol-poc');
+plot(edgSitgMapData, 'edg-sitg-map');
+plot(sedexMessageHandlerData, 'sedex-Message-Handler');
+plot(logChainerData, 'log-chainer');
+plot(squeletteGithubData, 'squelette-github');
+plot(joursFeriesData, 'jours-feries');
+plot(referentielsOfsData, 'referentiels-ofs');
+plot(blockchainDocumentHorodateurData, 'blockchain-document-horodateur');
+plot(pythonRedhatVirtualizationData, 'python-redhat-virtualization');
+plot(ginaLdapClientData, 'gina-ldap-client');
+plot(xxegePurgeData, 'xxege-purge');
+plot(gitSyncData, 'git-sync');
+plot(wlsSystemdScriptsData, 'wls-systemd-scripts');
+
+function plot(data, name) {
+      d3.select('.myClass')
+          .append('span')
+          .text('from D3.js')
+          .attr('style', 'color:red');
+      d3.select('#list')
+          .selectAll('li')
+          .data([10, 20, 30, 25, 15])
+          .text(function(d) { return 'This is pre-existing element and the value is ' + d; })
+          .enter()
+          .append('li')
+          .text(function(d) { return 'This is dynamically created element and the value is ' + d; });
+      d3.select('body')
+          // make the background-color lightblue.transition()
+          .style('background-color', 'lightblue')
+          // make the background-color gray
+          .style('background-color', 'gray');
+      d3.select('h3')
+          .transition()
+          .style('color', 'green')
+          .duration('2000')
+          .transition()
+          .style('color', 'blue')
+          .duration('2000');
+
+      // set the dimensions and margins of the graph
+      const margin = {
+          top: 100,
+          right: 20,
+          bottom: 30,
+          left: 50};
+      const width = 960 - margin.left - margin.right;
+      const height = 500 - margin.top - margin.bottom;
+
+      // set the ranges (scales)
+      const x = d3.scaleTime().range([0, width]);
+      const y = d3.scaleLinear().range([height, 0]);
+
+      // define the line of the number of unique cloners
+      const clonersLine = d3
+          .line()
+          .x(function(d) { return x(d[0]); })
+          .y(function(d) { return y(d[1]); });
+
+      // define the line of the number of unique visitors
+      const visitorsLine = d3
+          .line()
+          .x(function(d) { return x(d[0]); })
+          .y(function(d) { return y(d[2]); });
+
+      // append the svg object to the body of the page
+      // append a 'group' element to 'svg'
+      // move the 'group' element to the top left margin
+      const svg = d3
+          .select('body')
+          .append('svg')
+          .attr('width', width + margin.left + margin.right)
+          .attr('height', height + margin.top + margin.bottom)
+          .append('g').attr(
+                  'transform',
+                  'translate(' + margin.left + ',' + margin.top + ')');
+
+      // format the data
+      const parseTime = d3.timeParse('%b %Y');   // e.g., 'Oct 2019'
+      data.forEach(function(d) {
+        console.log('d :', d[0], d[1], d[2], parseTime(d[0]));
+        d[0] = parseTime(d[0]);
+        d[1] = d[1];
+        d[2] = +d[2];
+      });
+
+      // scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d[0]; }));
+      y.domain([0, d3.max(data, function(d) { return Math.max(d[1], d[2]); })]);
+
+      // add the clonersLine path
+      svg.append('path')
+          .data([data])
+          .attr('class', 'cloners')
+          .attr('d', clonersLine);
+
+      // add the visitorsLine path
+      svg.append('path')
+          .data([data])
+          .attr('class', 'visitors')
+          .attr('d', visitorsLine);
+
+      // add the x axis
+      svg.append('g')
+          .attr('transform', 'translate(0,' + height + ')')
+          .call(d3.axisBottom(x));
+
+      // add the y axis
+      svg.append('g')
+          .call(d3.axisLeft(y));
+
+      // add a legend
+      svg.append('text')
+          .attr('x', width / 2)
+          .attr('y', 0 - margin.top / 5)
+          .attr('text-anchor', 'middle')
+          .attr('class', 'legend')
+          .text(name);
+}
